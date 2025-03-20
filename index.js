5cd1ba127c35c13c8e115f341e17d104f58f5b28
Rohan Rajesh <dev.rohan.rajesh@gmail.com>
2025-03-20 00:17:52 -0700

    fix: added cors headers

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const port = 8080;
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Custom-Header"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
+});
+
 const generateOtp = () => {
   const digits = "0123456789";
   const otpLength = 4;
